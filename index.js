484d5907150ecf446fff41b428e449d2fad27107
Konrad Goldammer <konradgoldammer@gmail.com>
2022-01-03 19:47:34 +0100

    Improve image creation massively

--- index.js
+++ index.js
@@ -7,39 +7,47 @@ const groupSubStrings = (subStrings, font, bgHeight, bgWidth) =>
         const subs = [...subStrings];
 
         let maxWidth = 0;
+        let sumHeight = 0;
 
         const sumMinMargins = (bgWidth / 100) * 10 * 2; // 10% on each side
         const maxAllowedWidth = bgWidth - sumMinMargins;
 
-        const groups = [];
+        const rows = [];
 
         do {
-          let totalWidth = 0;
-          const group = [];
+          let rowWidth = 0;
+          const columns = [];
 
           for (sub of subs) {
-            const subWidth = await Jimp.measureText(font, sub);
-            if (totalWidth + subWidth < maxAllowedWidth) {
-              group.push(sub);
-              totalWidth += await Jimp.measureText(font, sub);
+            const columnWidth = await Jimp.measureText(font, sub);
+            if (rowWidth + columnWidth < maxAllowedWidth) {
+              columns.push({ content: sub, width: columnWidth });
+              rowWidth += await Jimp.measureText(font, sub);
               continue;
             }
-            if (group.length === 0) {
+            if (columns.length === 0) {
               return reject(new Error("Word too long - cannot be used"));
             }
             break;
           }
 
-          subs.splice(0, group.length);
+          subs.splice(0, columns.length);
 
-          if (totalWidth > maxWidth) {
-            maxWidth = totalWidth;
+          if (rowWidth > maxWidth) {
+            maxWidth = rowWidth;
           }
 
-          groups.push(group);
+          const height = await Jimp.measureTextHeight(
+            font,
+            columns.map((column) => column.content).join("")
+          );
+
+          sumHeight += height;
+
+          rows.push({ columns, width: rowWidth, height });
         } while (subs.length !== 0);
 
-        resolve({ groups, maxWidth });
+        resolve({ rows, maxWidth, sumHeight });
       } catch (err) {
         reject(err);
       }
@@ -62,20 +70,42 @@ const createImage = (text) =>
 
         const bg = await Jimp.read("images/bg.jpg");
         const logo = await Jimp.read("images/logo.jpg");
-        const font = await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
 
         const bgHeight = 1080;
         const bgWidth = 1080;
         const logoHeight = 120;
         const logoWidth = 120;
 
-        const subStrings = text.split(" ");
+        const subStrings = text.replaceAll(" ", " ~").split("~");
 
         console.log(subStrings);
-        const obj = await groupSubStrings(subStrings, font, bgHeight, bgWidth);
-        console.log(obj);
 
-        bg.print(font, 0, 0, text);
+        const { rows, maxWidth, sumHeight } = await groupSubStrings(
+          subStrings,
+          font,
+          bgHeight,
+          bgWidth
+        );
+
+        let marginTop = (bgHeight - sumHeight) / 2;
+        for (row of rows) {
+          let marginLeft = (bgWidth - maxWidth) / 2;
+          for (column of row.columns) {
+            bg.print(font, marginLeft, marginTop, column.content);
+            if (column.content.includes("penis")) {
+              const penis = column.content.substring(0, 5); // bc penis is 5 chars long
+              bg.print(
+                await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK),
+                marginLeft,
+                marginTop,
+                penis
+              );
+            }
+            marginLeft += column.width;
+          }
+          marginTop += row.height;
+        }
 
         logo.resize(logoHeight, logoWidth);
 
@@ -96,8 +126,7 @@ const createImage = (text) =>
 
 (async function () {
   try {
-    await createImage("Behind every great man there is a great penis.");
-    await createImage("a");
+    await createImage("I am a penis quote.");
     // console.log(
     //   await Jimp.measureText(await Jimp.loadFont(Jimp.FONT_SANS_14_BLACK), "s")
     // );
